8914a24097551f3dc69db945dcd9776ece283226
OSAMAGHAFFARTKOJL <116808680+OSAMAGHAFFARTKOJL@users.noreply.github.com>
2024-03-10 16:31:56 +0500

    app.py

--- app.py
+++ app.py
@@ -7,19 +7,20 @@ import openai
 from transformers import pipeline
 
 class ChoiceException(Exception):
-    def __init__(self, value):
+    def init(self, value):
         self.value = value
 
-st.title("YouTube Transcript Translator")
+st.markdown("<h1 style='text-align: center;'>VidTranscribe Hub</h1>", unsafe_allow_html=True)
 
 
 input_link = st.text_input("Enter a valid YouTube Video Link: ")
+st.write("❌ Video will not be more then 1 minute")
 target_language = st.text_input("Enter the  language  for translation (e.g. French): ")
 
 if st.button("Generate Transcript"):
     try:
         yt = pytube.YouTube(input_link)
-        st.write("⌛ We are working on generating your transcript... Please wait...")
+        
 
         # Get the audio stream
         audio = yt.streams.filter(only_audio=True).first()
@@ -31,11 +32,12 @@ if st.button("Generate Transcript"):
         wav_filename = os.path.splitext(audio_path)[0] + '.wav'
         os.system(f'ffmpeg -i "{audio_path}" "{wav_filename}"')
 
-        st.write("✅ TRANSCRIPT GENERATED SUCCESS!")
-
+        
+        st.write("Orignal Voice")
         # Display audio player
         st.audio(wav_filename, format='audio/wav')
-
+        
+        st.info("⌛ We are working on generating your transcript... Please wait...")
         # Provide a button to download the audio file
         #st.markdown(f'<a href="{wav_filename}" download>Download Audio</a>', unsafe_allow_html=True)
 
@@ -46,7 +48,7 @@ if st.button("Generate Transcript"):
         # Ask the user for the target language
 
         # Set up OpenAI API key
-        openai.api_key = "Enter Your OpenAI API Key"
+        openai.api_key = "Enter your Open AI API Key"
 
         translation_prompt = f"Translate the following English text to {target_language}: \n\n {result}"
         response = openai.ChatCompletion.create(
@@ -62,12 +64,12 @@ if st.button("Generate Transcript"):
         st.write("--------------------------------------------------------")
         st.write("--------------------------------------------------------")
      
-        st.write("Original Summary: ")
+        st.subheader("*Original Summary:*")
         st.write(result)
       
-        st.write(f"Translated Summary (to {target_language}): ")
+        st.subheader(f"*Translated Summary to {target_language}:*")
         st.write(translated_result)
-        st.download_button('Download  text', translated_result)
-
+        st.download_button('*Download  text*', translated_result)
+        st.markdown("<p style='text-align: center; color: green;'>✅ TRANSCRIPT GENERATED SUCCESS!</p>", unsafe_allow_html=True)
     except pytube.exceptions.RegexMatchError:
         st.error("🛑 Invalid URL! 🛑")
